e8e1c719fd05d4c9e785d6a69165c024c71e46e1
Vakho <vakho10@gmail.com>
2023-09-05 16:36:33 +0400

    Add static-website project to first lesson

---
+++ 1. Introduction/static-website/src/main/java/ge/tsu/StaticWebsiteApplication.java
@@ -0,0 +1,13 @@
+package ge.tsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticWebsiteApplication.class, args);
+	}
+
+}
---
+++ 1. Introduction/static-website/src/test/java/ge/tsu/StaticWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.tsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
